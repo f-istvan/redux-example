9fe8e354ba83b007236539b99941a5a77a0b3b6e
f-istvan <f.istvan1984@gmail.com>
2017-06-18 16:29:51 +0200

    add todo render view

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,23 @@
 import { createStore } from 'redux/';
 import { todoApp } from './reducers';
 import * as todoActions from './todoActions';
+import TodoSubscriber from './TodoSubscriber';
 
 let store = createStore(todoApp);
 
 (<any>window).addTodo = function () {
     let todoText = (<HTMLInputElement> document.getElementById('todoText')).value;
-    console.log(store.getState());
+    //console.log(store.getState());
     store.dispatch(todoActions.addTodo(todoText));
-    console.log(store.getState());
+    //console.log(store.getState());
 };
 
 (<any>window).removeTodo = function (todoText:string) {
-    console.log(store.getState());
+    //console.log(store.getState());
     store.dispatch(todoActions.removeTodo(todoText));
-    console.log(store.getState());
+    //console.log(store.getState());
 };
+
+
+let todoSubscriber = new TodoSubscriber(store);
+store.subscribe(todoSubscriber.handleTodoAction);
--- src/todoReducer.ts
+++ src/todoReducer.ts
@@ -5,11 +5,20 @@ const initState:any = [];
 export const todos = (state = initState, action:any) => {
     switch (action.type) {
     case actions.ADD_TODO:
-        return [...state, action.text];
+        let text = action.text.trim();
+        if (text != '' && text != undefined && isUniqueElem(state, text)) {
+            return [...state, text];
+        } else {
+            return state;
+        }
     case actions.REMOVE_TODO:
         return state
             .filter((e:string) => e !== action.text);
     default:
         return initState;
+    }
+
+    function isUniqueElem(arr:any[], elem:any) {
+        return arr.indexOf(elem) < 0;
     }
 };
